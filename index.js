cfefd285b86192d2a04487b01de7a63ec4fce319
broglhub <broglhubdonate@gmail.com>
2020-06-19 16:42:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -55,16 +55,33 @@ client.on("message", async message => {
 if(command === "8ball") {
 	if(!args[0]) return message.reply(`please ask a question`);
 	let replies = [
-	'maybe',
-	'no'
-	'likely'
-	'not likely'
-	'idk, fuck you'
-	'fuck off'
-	'yes'
-	'of course'
-	'always'
-	'stfu?'
+        'Maybe.',
+	    'Certainly not.',
+	    'I hope so.',
+	    'Not in your wildest dreams.',
+    	'There is a good chance.',
+	    'Quite likely.',
+    	'I think so.',
+    	'I hope not.',
+    	'I hope so.',
+    	'Never!',
+    	'Pfft.',
+	    'Sorry, bucko.',
+    	'Hell, yes.',
+    	'Hell to the no.',
+    	'The future is bleak.',
+	    'The future is uncertain.',
+	    'I would rather not say.',
+    	'Who cares?',
+    	'Possibly.',
+    	'Never, ever, ever.',
+    	'There is a small chance.',
+    	'Yes!',
+    	'lol no.',
+    	'There is a high probability.',
+    	'What difference does it makes?',
+    	'Not my problem.',
+        'Ask someone else.'
 	];
 	
 	let result = Math.floor((Math.random() * replies.length));
