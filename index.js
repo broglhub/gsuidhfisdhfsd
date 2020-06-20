702d10c1e83d63457c6fdbf1e7a44348cce531d9
broglhub <broglhubdonate@gmail.com>
2020-06-20 12:16:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -278,7 +278,9 @@ if(command === "suggest") {
 }
 
 client.on('message', function(message){
-if(message.channel.type === "dm") console.log(`[${message.author.username}] ${message.content}`); 
+if(message.channel.type === "dm")
+var logger = client.channels.get("723842832921657374")
+logger.send(`[${message.author.username}] ${message.content}`); 
 });
 
 	
