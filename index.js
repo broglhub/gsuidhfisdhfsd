3d8e85ed20781c1d69c09ef9949aca90ae3926d5
broglhub <broglhubdonate@gmail.com>
2020-06-20 13:06:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -294,7 +294,7 @@ if(command === "poll") {
 		.setFooter("made by Chaotic Mind#0666", "https://cdn.discordapp.com/avatars/655714844695330854/a_ac9969af8c3d41eeac55fc134b0412a4.gif?size=2048")
 		.setThumbnail("https://cdn.discordapp.com/avatars/714874905669402634/b538a848a7825a2d5ee1bae150c241a4.png?size=2048")
 		.setTimestamp();
-		await poll.send({poll});
+		await polls.send({poll});
 		poll.react('722730090533748796')
 		poll.react('722730090307518566')
 }
