9a01fae12f0b8da4873e5e7e1a5aeba66e0a45db
broglhub <broglhubdonate@gmail.com>
2020-06-20 11:33:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -247,7 +247,7 @@ members: ${message.guild.memberCount}`);
 
 if(command === "av") {
 	let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-	if(!member) member = message.author.tag
+	if(!member) member = message.author
 const embed = new Discord.RichEmbed()
   .setTitle("Here you go!")
   .setAuthor(`${member.user.tag}`, `${member.user.avatarURL}`)
