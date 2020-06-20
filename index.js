7cfda49c88b4722d9e6acea0b99eaef184854cda
broglhub <broglhubdonate@gmail.com>
2020-06-20 21:28:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -267,7 +267,6 @@ if(command === "ruleview") {
   .setDescription("Read these first before complaining about being punished!")
   .setFooter("Made by Chaotic Mind#0666", "https://cdn.discordapp.com/avatars/655714844695330854/a_ac9969af8c3d41eeac55fc134b0412a4.gif?size=2048")
   .setThumbnail("https://cdn.discordapp.com/avatars/714874905669402634/b538a848a7825a2d5ee1bae150c241a4.png?size=2048")
-  .setTimestamp()
   .addField("Spamming", "Don't spam. spamming will result into mutes or further punishments.", true)
   .addField("NSFW content", "Do not post any animal abuse videos, child abuse, gore in any channels. no nsfw outside the nsfw channel", true)
   .addField("Voice and audio", "do not post any loud / earrape audios as well as playing earrapes on voicechat.", true)
