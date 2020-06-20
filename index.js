08280f3ab30c181034a6af0455976e6e9e3a047d
broglhub <broglhubdonate@gmail.com>
2020-06-20 09:03:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -237,7 +237,7 @@ const embed = new Discord.RichEmbed()
   .setColor(0x00AE86)
   .setFooter("made by Chaotic Mind#0666", "https://cdn.discordapp.com/avatars/655714844695330854/a_ac9969af8c3d41eeac55fc134b0412a4.gif?size=2048")
   .setImage(`${member.user.avatarURL}`)
-  .setThumbnail("http://i.imgur.com/p2qNFag.png")
+  .setThumbnail("https://cdn.discordapp.com/avatars/714874905669402634/b538a848a7825a2d5ee1bae150c241a4.png?size=2048")
   .setTimestamp()
  
   message.channel.send({embed});
