d90278bba8366c7152e371a8d970491648ad95d3
broglhub <broglhubdonate@gmail.com>
2020-06-21 12:02:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ const embed = new Discord.RichEmbed()
 	.addField("Status:", `${user.presence.status}`, true)
 	.addField("Game:", `${user.presence.game ? user.presence.game.name : 'None'}`, true)
 	.addField("Bot:", `${user.bot}`, true)
-	.addField("Roles:", member.roles.map(roles => `${roles}`).join(', '), true)
+	.addField('Roles', message.member.roles.filter(role => role.name !== "@everyone").map(role => role.name).join(", "))
 	.setFooter(`made by Chaotic Mind#0666`, "https://cdn.discordapp.com/avatars/655714844695330854/a_ac9969af8c3d41eeac55fc134b0412a4.gif?size=2048")
 	message.channel.send({embed: userinf});
 	}	
