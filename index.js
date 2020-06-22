47e7095764f189b843742100f40d74223531d5b2
broglhub <66021111+broglhub@users.noreply.github.com>
2020-06-22 09:08:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -522,7 +522,7 @@ if(command === "suckadick") {
 }
 
 if(command === "jail") {
-	let user = message.mentions.members.first() || message.guild.members.get(args[0]);
+	let user = message.mentions.members.first() || message.guild.members.get(args[0]) || message.author;
 	message.channel.send(`<:minecraft_iron_bars:722913387020943541><:minecraft_iron_bars:722913387020943541><:minecraft_iron_bars:722913387020943541><:minecraft_iron_bars:722913387020943541><:minecraft_iron_bars:722913387020943541><:minecraft_iron_bars:722913387020943541><:minecraft_iron_bars:722913387020943541><:minecraft_iron_bars:722913387020943541>
 <:minecraft_iron_bars:722913387020943541> <@!${user.id}> <:minecraft_iron_bars:722913387020943541>
 <:Minecraft_Stone:722917154340733078><:Minecraft_Stone:722917154340733078><:Minecraft_Stone:722917154340733078><:Minecraft_Stone:722917154340733078><:Minecraft_Stone:722917154340733078><:Minecraft_Stone:722917154340733078><:Minecraft_Stone:722917154340733078><:Minecraft_Stone:722917154340733078><:Minecraft_Stone:722917154340733078>`)
