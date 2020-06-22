d92a83bcb400e5c05d7eea1573972caac4b4d4d4
broglhub <broglhubdonate@gmail.com>
2020-06-22 15:36:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ fs.readdir("./events/", (_err, files) => {
 		if (!file.endsWith(".js")) return;
 		const event = require(`./events/${file}`);
 		let eventName = file.split(".")[0];
-		console.log(`${loaded ${eventName}`);
+		console.log(`loaded ${eventName}`);
 		client.on(eventName, event.bind(null, client));
 		delete require.cache[require.resolve(`./events/${file}`)];
 	});
