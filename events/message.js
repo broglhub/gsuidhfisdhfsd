8ff2a45e67ca549b994924967678c5f1781f86b5
broglhub <broglhubdonate@gmail.com>
2020-06-22 15:38:21 +0200

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,4 +1,4 @@
-module.exports = (client, message) => 
+module.exports = (client, message) => {
 if(message.author.bot) return;
 if(message.content.indexOf(client.config.prefix) !== 0) return;
 const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
