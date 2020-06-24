7a50c79a75c22346d04fcb31cafcea1cd10e46f5
broglhub <66021111+broglhub@users.noreply.github.com>
2020-06-24 20:58:57 +0200

    Update emoji.js

--- commands/emoji.js
+++ commands/emoji.js
@@ -1,15 +1,15 @@
-//const Discord = require('discord.js')
+const Discord = require('discord.js')
 
-//module.exports = {
-//name: 'emoji',
-//aliases: ["newemoji", "createemoji"],
-//description: "makes a new emoji",
-//async execute(client, config, message, args) {
-//    if(!message.member.roles.some(r=>["Owner"].includes(r.name)) ) return;
-//    let emoji = args.slice(0).join(' ');
-//	let emojiname = args.slice(1).join(' ');
-//	await message.guild.createEmoji(`${emoji}`, `${emojiname}`)
-//	.then(emoji => message.channel.send(`created emoji succesfully`))
-//	.catch(console.error);
-//},
-//};
+module.exports = {
+name: 'emoji',
+aliases: ["newemoji", "createemoji"],
+description: "makes a new emoji",
+async execute(client, config, message, args) {
+    if(!message.member.roles.some(r=>["Owner"].includes(r.name)) ) return;
+        let emoji = args.slice(0).join(' ');
+	let emojiname = args.slice(1).join(' ');
+	await message.guild.createEmoji(`${emoji}`, `${emojiname}`)
+	.then(emoji => message.channel.send(`created emoji succesfully`))
+	.catch(console.error);
+},
+};
