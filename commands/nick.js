d73aa08dd9d82ba3b2ad012f4a8b53b4a5f72d7c
broglhub <broglhubdonate@gmail.com>
2020-06-25 16:06:10 +0200

    Update nick.js

--- commands/nick.js
+++ commands/nick.js
@@ -6,7 +6,7 @@ aliases: ["user", "setnick"],
 description: "change the target's username",
 async execute(client, config, message, args) {
     if(!message.member.roles.some(r=>["trial moderator", "moderator", "head moderator", "Admin", "head administrator", "Co-Owner", "Owner"].includes(r.name)) )
-    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.author;
     let name = args.slice(1).join(' ');
    message.delete().catch(O_o=>{});
     member.setNickname(`${name}`)
