3f0b37e0daad1fe0304c17c5697cab6312eca023
broglhub <66021111+broglhub@users.noreply.github.com>
2020-06-25 09:06:13 +0200

    Update emoji.js

--- commands/emoji.js
+++ commands/emoji.js
@@ -8,9 +8,9 @@ async execute(client, config, message, args) {
     if(!message.member.roles.some(r=>["Owner"].includes(r.name)) ) return;
         let emoji = args.slice(0).join(' ');
 	let emojiname = args.slice(1).join(' ');
-	var m = await message.guild.createEmoji(`${emoji}`, `${emojiname}`)
+	 let emote = await message.guild.createEmoji(`${emoji}`, `${emojiname}`)
 	.then(emoji => message.channel.send(`created emoji succesfully`))
 	.catch(console.error)
-	await message.channel.send(`${emojiname} ${m.id}`);
+	await message.channel.send(`${emojiname} ${emote.id}`);
 },
 };
