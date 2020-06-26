55e2fd88f6a0fcb3a68259aa7874d564f1244774
shadowlords <66021111+shadowlords@users.noreply.github.com>
2020-06-26 09:50:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ client.on('message', async message => {
 	if(message.channel.type === "dm")
 var logchannel = client.channels.get('724265992729002045')
 await logchannel.send(`[${message.author.tag}]: ${message.content}`);
-}
+});
 
 client.on('message', async message => {
 	if(message.author.bot) return;
