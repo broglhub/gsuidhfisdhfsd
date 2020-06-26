52450199c1487e7d1cd7ae1ecedb0fff5f5291f9
shadowlords <66021111+shadowlords@users.noreply.github.com>
2020-06-26 10:11:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ client.on("messageDelete", async msg => {
     .addField("Author", msg.author.tag, true)
     .addField("Channel", msg.channel, true)
     .addField("Message", msg.content)
-    .addField("Deleted by", entry.executor.tag)
+    .addField("Deleted by", entry.executor)
     .setFooter(`Message ID: ${msg.id} | Author ID: ${msg.author.id}`);
 	
 	var logchanneldel = client.channels.get('724265992729002045')
