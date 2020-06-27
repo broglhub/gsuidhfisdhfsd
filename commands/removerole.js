a72eae4210db6624c2b19c0128dfedf0aebe5da1
broglhub <broglhubdonate@gmail.com>
2020-06-27 12:09:45 +0200

    Update removerole.js

--- commands/removerole.js
+++ commands/removerole.js
@@ -9,7 +9,7 @@ async execute(client, config, message, args) {
     let member = message.mentions.users.first() || message.guild.members.get(args[0]);
     if(!member) return message.channel.send("please give a valid user");
     let rname = message.content.split(" ").splice(2).join(" ");
-    let role = message.guild.roles.cache.find(val => val.name === rname);
+    let role = message.guild.roles.find(val => val.name === rname);
     if(!role) return message.channel.send("didn't find role");
     member.removeRole(role).catch(e => {
         return message.channel.send("error found okbye");
