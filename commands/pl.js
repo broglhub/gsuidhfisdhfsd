9a03734f5974a236f3fd5915c1eff8dcffcbee44
broglhub <broglhubdonate@gmail.com>
2020-06-28 15:32:05 +0200

    Update pl.js

--- commands/pl.js
+++ commands/pl.js
@@ -19,5 +19,71 @@ async execute(client, config, message, args) {
         sewers
         back area (unused part)`
     }});
+    await prischannel.send({embed: {
+        color: 3447003,
+        title: "locals",
+        description: `guards
+        inmates
+        neutral
+        criminal
+        Fog
+        Transparent
+        Really black
+        Earth green
+        `
+    }});
+    await prischannel.send({embed: {
+        color: 3447003,
+        title: "global shit",
+        description: `kill prisoner
+        kill guards
+        kill neutral
+        kill criminals
+        loopkill prisoners
+        loopkill guards
+        loopkill criminals
+        loopkill neutral
+        kill all
+        arrest criminals
+        textbox PLRS
+        makecrim
+        spam arrest off
+        spam arrest on (both as toggle)
+        kill
+        taze
+        arrest
+        loopkilloff
+        loopkillon(toggle too)
+        goto
+        view on
+        view off(toggle)
+        age
+        id
+        os
+        `
+    }});
+    await prischannel.send({embed: {
+        color: 3447003,
+        title: "toggles",
+        description: `rapid punch
+        infinite stamina
+        rainbow bullets
+        auto respawn
+        killaura
+        fullbright`
+    }});
+    await prischannel.send({embed: {
+        color: 3447003,
+        title: "utility",
+        description: `rejoin
+        global admin
+        textbox for ^
+        suicide
+        no doors
+        backwards guns
+        retarded guns
+        gunmods
+        `
+    }});
 },
 };
