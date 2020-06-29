f1ad7e7f3f5f11cc56e22d80363d3eb4df74c757
shadowlords <66021111+shadowlords@users.noreply.github.com>
2020-06-29 08:35:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -89,6 +89,7 @@ if(message.content.includes('<@!714874905669402634>')) {
 });
 
 client.on('messageUpdate', async (oldMessage, newMessage) => {
+	if(message.author.bot) return;
 	let editembed = new Discord.RichEmbed()
 	.setTitle("**EDITED MESSAGE**")
 	.setColor(0x800000)
