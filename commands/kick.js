9b04b245b37fbfb6a102ad3e86304eeab32111f3
shadowlords <66021111+shadowlords@users.noreply.github.com>
2020-06-29 09:05:45 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -33,8 +33,7 @@ async execute(client, config, message, args) {
     color: 3447003,
     description: `kicked ${member.user.tag} succesfully!`
   }});
-      var logs = client.channels.get("724265992729002045");
-      logs.send({embed: {
+      logchannel.send({embed: {
     color: 3447003,
     description: `user ${member.user.tag} was kicked by ${message.author.tag} for the reason: ${reason}`
   }});
