382e1e5085f4c5c4864979023b9718f9e09b35da
shadowlords <66021111+shadowlords@users.noreply.github.com>
2020-06-29 10:03:47 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -10,7 +10,7 @@ module.exports = {
         .setTitle("Pong!")
         .setColor(0x800000)
         .setTimestamp()
-        .addField("Bot speed", `-${m.createdTimestamp - message.createdTimestamp}ms`, true)
+        .addField("Bot speed", `${m.createdTimestamp - message.createdTimestamp}ms`, true)
         .addField("Api speed", `${Math.round(client.ping)}ms`, true)
         .setFooter(`made by Chaotic Mind#0666`, `https://cdn.discordapp.com/avatars/655714844695330854/a_ac9969af8c3d41eeac55fc134b0412a4.gif?size=2048`);
         m.edit({embed: pong});
