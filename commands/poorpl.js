42e14513181a61b4a18b207d53e67c32b51625ad
shadowlords <66021111+shadowlords@users.noreply.github.com>
2020-06-29 11:19:05 +0200

    Update poorpl.js

--- commands/poorpl.js
+++ commands/poorpl.js
@@ -5,7 +5,6 @@ name: 'poorpl',
 aliases: ["purple", "dood"],
 description: "yes",
 async execute(client, config, message, args) {
-if(message.author.bot) return;
 message.channel.send(`🟪🟪🟪🟪🟪🟪🟪
 🟪🟪🟪🟪🟪🟪🟪
 🟪⬛⬜🟪⬛⬜🟪
