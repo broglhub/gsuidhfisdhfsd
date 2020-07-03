26ee89377d924eec0188e70fd716ee1c69e1a960
shadowlords <66021111+shadowlords@users.noreply.github.com>
2020-07-03 08:53:31 +0200

    Update av.js

--- commands/av.js
+++ commands/av.js
@@ -5,13 +5,13 @@ module.exports = {
     aliases: ['avatar', 'pfp'],
     description: 'display your avatar',
     execute(client, config, message, args) {
-        const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.author;
+        let user = message.mentions.members.first() || message.guild.members.get(args[0]) || message.author;
         let av = new Discord.RichEmbed()
         .setTitle("Here you go fam!")
-        .setAuthor(`${member.tag}`, `${member.avatarURL}`)
+        .setAuthor(`${user.tag}`, `${user.avatarURL}`)
         .setColor(0x800000)
         .setFooter("Made by Chaotic Mind#0666", "https://cdn.discordapp.com/avatars/655714844695330854/a_ac9969af8c3d41eeac55fc134b0412a4.gif?size=2048")
-        .setImage(`${member.avatarURL}`)
+        .setImage(`${user.avatarURL}`)
         .setThumbnail("https://cdn.discordapp.com/avatars/714874905669402634/b538a848a7825a2d5ee1bae150c241a4.png?size=2048")
         .setTimestamp()
         message.channel.send({embed: av});
