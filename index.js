ba3c527127cd325df51e5fe69fa98f3db2c0fac3
shadowlords <66021111+shadowlords@users.noreply.github.com>
2020-07-03 09:27:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const config = require('./config.json');
 const chalk = require('chalk');
 const Constants = require('discord.js/src/util/Constants.js')
-Constants.DefaultOptions.ws.Properties.$browser = 'Discord Android'
+Constants.DefaultOptions.ws.properties.$browser = 'Discord Android'
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
