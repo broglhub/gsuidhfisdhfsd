8b8c6a53138993980d3bf79bd238cc99356fec84
shadowlords <66021111+shadowlords@users.noreply.github.com>
2020-07-03 08:55:04 +0200

    Update av.js

--- commands/av.js
+++ commands/av.js
@@ -8,10 +8,10 @@ module.exports = {
         let user = message.mentions.members.first() || message.guild.members.get(args[0]) || message.author;
         let av = new Discord.RichEmbed()
         .setTitle("Here you go fam!")
-        .setAuthor(`${user.tag}`, `${user.avatarURL}`)
+        .setAuthor(`${user.tag}`, `${user.member.avatarURL}`)
         .setColor(0x800000)
         .setFooter("Made by Chaotic Mind#0666", "https://cdn.discordapp.com/avatars/655714844695330854/a_ac9969af8c3d41eeac55fc134b0412a4.gif?size=2048")
-        .setImage(`${user.avatarURL}`)
+        .setImage(`${user.member.avatarURL}`)
         .setThumbnail("https://cdn.discordapp.com/avatars/714874905669402634/b538a848a7825a2d5ee1bae150c241a4.png?size=2048")
         .setTimestamp()
         message.channel.send({embed: av});
