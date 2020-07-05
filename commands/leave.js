ac2e301510740703c6edae27be86ac19a3e03c07
broglhub <broglhubdonate@gmail.com>
2020-07-05 13:19:38 +0200

    Update leave.js

--- commands/leave.js
+++ commands/leave.js
@@ -6,6 +6,6 @@ aliases: ["fuckoff"],
 description: "leaves vc yes",
 async execute(client, config, message, args) {
 if(!message.member.voice.channel) return message.channel.send("no");
-client.leaveVoiceChannel(message.author.voiceChannel);
+client.voiceChannel.leave(message.author.voiceChannel.id);
 },
 };
