ec6b12e0b99b7753458309546038f064327482c9
broglhub <broglhubdonate@gmail.com>
2020-07-05 11:42:39 +0200

    a

--- commands/meme.js
+++ commands/meme.js
@@ -1,58 +1,26 @@
 const Discord = require('discord.js')
-const https = require('https')
-const url = 'https://www.reddit.com/r/meme/hot/.json?limit=100'
+const got = require('got')
 
 module.exports = {
 name: 'meme',
 description: "gib meme",
 async execute(client, config, message, args) {
-    https.get(url, (result) => {
-        var body = ''
-        result.on('data', (chunk) => {
-            body += chunk
-        })
-
-        result.on('end', () => {
-            var response = JSON.parse(body)
-            var index = response.data.children[Math.floor(Math.random() * 99) + 1].data
-
-            if (index.post_hint !== 'image') {
-
-                var text = index.selftext
-                const textembed = new Discord.MessageEmbed()
-                    .setTitle(subRedditName)
-                    .setColor(0x800000)
-                    .setDescription(`[${title}](${link})\n\n${text}`)
-                    .setURL(`https://reddit.com/${subRedditName}`)
-
-                message.channel.send(textembed)
-            }
-
-            var image = index.preview.images[0].source.url.replace('&amp;', '&')
-            var title = index.title
-            var link = 'https://reddit.com' + index.permalink
-            var subRedditName = index.subreddit_name_prefixed
-
-            if (index.post_hint !== 'image') {
-                const textembed = new Discord.RichEmbed()
-                    .setTitle(subRedditName)
-                    .setColor(0x800000)
-                    .setDescription(`[${title}](${link})\n\n${text}`)
-                    .setURL(`https://reddit.com/${subRedditName}`)
-
-                message.channel.send(textembed)
-            }
-            console.log(image);
-            const imageembed = new Discord.MessageEmbed()
-                .setTitle(subRedditName)
-                .setImage(image)
-                .setColor(0x800000)
-                .setDescription(`[${title}](${link})`)
-                .setURL(`https://reddit.com/${subRedditName}`)
-            message.channel.send(imageembed)
-        }).on('error', function (e) {
-            console.log('Got an error: ', e)
-        })
-    })
+    const embed = new Discord.RichEmbed();
+    got('https://www.reddit.com/r/memes/random/.json').then(response => {
+        let content = JSON.parse(response.body);
+        let permalink = content[0].data.children[0].data.permalink;
+        let memeUrl = `https://reddit.com${permalink}`;
+        let memeImage = content[0].data.children[0].data.url;
+        let memeTitle = content[0].data.children[0].data.title;
+        let memeUpvotes = content[0].data.children[0].data.ups;
+        let memeDownvotes = content[0].data.children[0].data.downs;
+        let memeNumComments = content[0].data.children[0].data.num_comments;
+        embed.addField(`${memeTitle}`, `[View thread](${memeUrl})`);
+        embed.setImage(memeImage);
+        embed.setFooter(`👍 ${memeUpvotes} 👎 ${memeDownvotes} 💬 ${memeNumComments}`);
+        msg.channel.send(embed)
+            .then(sent => console.log(`Sent a reply to ${sent.author.username}`))
+        console.log('Bot responded with: ' + memeImage);
+    }).catch(console.error);
 },
 };
