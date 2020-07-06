9c6df8100528b4840abc3405d9dc7cfbb3d3f5bd
broglhub <broglhubdonate@gmail.com>
2020-07-06 19:09:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ client.on("message", async message => {
 async function execute(message, serverQueue) {
   const args = message.content.split(" ");
 
-  const voiceChannel = message.member.voice.channel;
+  const voiceChannel = await message.member.voice.channel;
   if (!voiceChannel)
     return message.channel.send(
       "You need to be in a voice channel to play music!"
