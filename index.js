375662490eea01d9cfc91ceb6cbb38766ab0450c
broglhub <broglhubdonate@gmail.com>
2020-07-06 18:29:45 +0200

    asdasd

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const Constants = require('discord.js/src/util/Constants.js')
 Constants.DefaultOptions.ws.properties.$browser = 'Discord Android'
 const chalk = require('chalk');
 const got = require('got');
+const { SIGKILL } = require('constants');
+const queue = new Map();
+const ytdl = require("ytdl-core");
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
@@ -105,5 +108,99 @@ client.on("messageDelete", async msg => {
 	var logchannel = client.channels.get('727050928544546856');
     logchannel.send({embed}).catch()
 });
+
+async function execute(message, serverQueue) {
+    const args = message.content.split(" ");
+
+    const voiceChannel = message.member.voice.channel;
+    if (!voiceChannel)
+    return message.channel.send("you need to be in a voice channel to use this")
+}
+
+const songInfo = await ytdl.getInfo(args[1]);
+const song = {
+    title: songInfo.title,
+    url: songInfo.video_url,
+};
+
+if(!serverQueue) {
+
+} else {
+    serverQueue.songs.push(song);
+    console.log(serverQueue.songs);
+    return message.channel.send(`${song.title} added to the queue!`);
+}
+
+const queueConstruct = {
+    textChannel: message.channel,
+    voiceChannel: voiceChannel,
+    connection: null,
+    songs: [],
+    volume: 5,
+    playing: true,
+};
+
+queue.set(message.guild.id, queueConstruct);
+
+queueConstruct.songs.push(song);
+
+try {
+    var connaction = await voiceChannel.join();
+    queueConstruct.connection = connection;
+
+    play(message.guild, queueConstruct.songs[0]);
+} catch (err) {
+    console.log(err);
+    queue.delete(message.guild.id);
+    return message.channel.send(err);
+}
+
+function play(guild, song) {
+    const serverQueue = queue.get(guild.id);
+    if (!song) {
+        serverQueue.voiceChannel.leave();
+        queue.delete(guild.id)
+        return;
+    }
+}
+
+const dispatcher = serverQueue.connection
+    .play(ytdl(song.url))
+    .on("finish", () => {
+        serverQueue.songs.shift();
+        play(guild, serverQueue.songs[0]);
+    })
+    .on("error", error => console.error(error));
+dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+serverQueue.textChannel.send(`Start playing: **${song.title}**`);
+
+function skip(message, serverQueue) {
+    if (!message.member.voice.channel)
+      return message.channel.send(
+        "You have to be in a voice channel to stop the music!"
+      );
+    if (!serverQueue)
+      return message.channel.send("There is no song that I could skip!");
+    serverQueue.connection.dispatcher.end();
+  }
+
+  function stop(message, serverQueue) {
+    if (!message.member.voice.channel)
+      return message.channel.send(
+        "You have to be in a voice channel to stop the music!"
+      );
+    serverQueue.songs = [];
+    serverQueue.connection.dispatcher.end();
+  }
+if(message.content.startsWith(config.prefix + "play")) {
+    execute(message, serverQueue);
+    return;
+} else if(message.content.startsWith(config.prefix + "skip")) {
+    skip(message, serverQueue);
+    return;
+} else if(message.content.startsWith(config.prefix + stop)) {
+    stop(message, serverQueue);
+    return;
+}
 
 client.login(process.env.BOT_TOKEN);
