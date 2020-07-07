b98808a9c4bafa2d43607f5edcf9382e76800328
shadowlords <66021111+shadowlords@users.noreply.github.com>
2020-07-07 10:19:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -221,7 +221,8 @@ const fetchedLogs = await message.guild.fetchAuditLogs({
 	if(!whomst) return logchannel.send('couldn\'t figure out who deleted this message');
 	const { executor, target } = whomst;
 	if(target.id === message.author.id) return 
-		var lo = `${executor.tag}` else
+		var lo = `${executor.tag}`;
+	else return
 		var lo = `unknown`;
 
   let embed = new Discord.RichEmbed()
