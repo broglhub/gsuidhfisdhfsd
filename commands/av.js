54f7fdefbf29a14a3f22970d351c6e3e1c91791c
shadowlords <66021111+shadowlords@users.noreply.github.com>
2020-07-09 09:59:11 +0200

    Update av.js

--- commands/av.js
+++ commands/av.js
@@ -10,7 +10,7 @@ let user = message.guild.members.get(args[0]) || message.author;
         const av = new Discord.RichEmbed()
         .setTitle("Here you go fam!")
         .setAuthor(`${user.tag}`)
-        .setImage(`${user.member.avatarURL}`)
+        .setImage(`${user.avatarURL}`)
         .setColor(0x800000)
         .setFooter("Made by Chaotic Mind#0666", "https://cdn.discordapp.com/avatars/655714844695330854/a_ac9969af8c3d41eeac55fc134b0412a4.gif?size=2048")
         .setThumbnail("https://cdn.discordapp.com/avatars/714874905669402634/b538a848a7825a2d5ee1bae150c241a4.png?size=2048")
