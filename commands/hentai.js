5133879596de9ac904ee2bbcf888aab71aa87cd1
broglhub <broglhubdonate@gmail.com>
2020-07-13 09:35:51 +0200

    Update hentai.js

--- commands/hentai.js
+++ commands/hentai.js
@@ -10,13 +10,13 @@ async execute(client, config, message, args) {
     got('https://www.reddit.com/r/hentai/random/.json').then(response => {
         let content = JSON.parse(response.body);
         let permalink = content[0].data.children[0].data.permalink;
-        let memeUrl = `https://reddit.com${permalink}`;
+        let henURL = `https://reddit.com${permalink}`;
         let memeImage = content[0].data.children[0].data.url;
         let memeTitle = content[0].data.children[0].data.title;
         let memeUpvotes = content[0].data.children[0].data.ups;
         let memeDownvotes = content[0].data.children[0].data.downs;
         let memeNumComments = content[0].data.children[0].data.num_comments;
-        ass.addField(`${memeTitle}`, `[View thread](${memeUrl})`);
+        ass.addField(`${memeTitle}`, `[View thread](${henURL})`);
         ass.setImage(memeImage);
         ass.setFooter(`👍 ${memeUpvotes} 👎 ${memeDownvotes} 💬 ${memeNumComments}`);
         message.channel.send({embed: ass})
